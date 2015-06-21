e58a3d6e8ad2935bd021b32611575e757bdd078d
Vijay Anand <vijayanand@email.arizona.edu>
2015-06-21 13:48:33 -0700

    Assignment 2 submission
    This R file contains two functions one for getting and setting the
    matrix and it's inverse and the other for actual calculation and check
    if there is an already calculated value.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function is simply a storage for both matrix and it's transpose. So it has four things two for setting up matrix and its transpose and 
+## other two for getting the stored value of matrix and its transpose.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y){
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	settranspose <- function(t) m <<- t
+	gettranspose <- function() m
+	list(set = set, get = get, settranspose = settranspose, gettranspose = gettranspose)
 }
 
 
-## Write a short comment describing this function
+## m holds the inverse of matrix a that is supplied. At first it tries to get the value from makeCacheMatrix which contins a list of values.
+## in case it returns an empty value this function calculates the transpose and also saves it to the list of makeCacheMatrix for future use.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$gettranspose()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	matrix <- x$get()
+	m <- solve(matrix)
+	x$settranspose(m)
+	m
 }
